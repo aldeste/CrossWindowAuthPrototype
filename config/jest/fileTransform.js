0994cbe2f0d17085025cad43c26ca9c0d94babb7
Alexander D'Este <alexanderdeste@gmail.com>
2017-04-24 10:45:12 +0200

    jest mocks

---
+++ config/jest/fileTransform.js
@@ -0,0 +1,11 @@
+"use strict";
+// Transform jest file imports to return filename as string
+// http://facebook.github.io/jest/docs/tutorial-webpack.html
+
+const path = require("path");
+
+module.exports = {
+  process(src, filename) {
+    return `module.exports = ${JSON.stringify(path.basename(filename))};`;
+  }
+};
