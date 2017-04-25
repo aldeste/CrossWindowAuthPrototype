fb213658cbe4bc842c109e9e2244dc8f231346c1
Alexander D'Este <alexanderdeste@gmail.com>
2017-04-25 09:54:30 +0200

    Encapsulate index test in describe

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,10 @@
-require("./");
-
 // We don't need to run each file,
 // we just check that it's included
-jest.mock("./server/main", () => null);
-jest.mock("babel-register", () => null);
-jest.mock("dotenv", () => ({ config: () => null }));
+jest.mock("./server/main", () => "main.js");
+jest.mock("dotenv", () => ({ config: () => "dotenv.config" }));
+jest.mock("babel-register", () => "babel-register");
 
 describe("index file", () => {
+  require("./");
   it("includes all setup dependencies", done => done());
 });
