0ac769d2bd65f9140ad1c209c561e0c97600c506
Alexander D'Este <alexanderdeste@gmail.com>
2017-04-26 14:36:57 +0200

    Remove reduntant files

--- example/src/StyleVariables.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-import React from "React";
-import StyleVariables from "./StyleVariables";
-
-test("StyleVariables renders correctly", () => {
-  const tree = renderer.create(<StyleVariables />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
