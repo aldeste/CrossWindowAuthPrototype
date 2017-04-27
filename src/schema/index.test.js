d98717ab76d60571fd846adbbf4650994d001f54
Alexander D'Este <alexanderdeste@gmail.com>
2017-04-27 16:10:17 +0200

    Insert callback in it function

--- src/schema/index.test.js
+++ src/schema/index.test.js
@@ -1,5 +1,5 @@
 import index from "./";
 
 describe("Schema", () => {
-  it("exports a GraphQL schema", expect(index).toBeDefined());
+  it("exports a GraphQL schema", () => expect(index).toBeDefined());
 });
