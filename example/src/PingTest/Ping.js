e176cddee11621e4133b7ea7e088f51cddb30c17
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-03 13:30:53 +0200

    Move ping test component to folder

---
+++ example/src/PingTest/Ping.js
@@ -0,0 +1,59 @@
+// @flow
+import React from "react";
+import { View, Title, Button, Text } from "../Tags";
+
+type State =
+  | {}
+  | {
+      user: string,
+      time?: string
+    };
+
+type Props = { token?: ?string };
+
+export async function resolveToken(token: ?string): Promise<Object> {
+  if (token) {
+    const user = await fetch("/connect", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+      mode: "cors",
+      cache: "default",
+      body: JSON.stringify({ token })
+    }).then(response => response.json());
+
+    // window.postMessage(
+    //   { type: "AuthVerificationConnection", data: user },
+    //   "http://localhost:4000"
+    // );
+    return user;
+  }
+  return {};
+}
+
+export default class extends React.PureComponent<*, Props, State> {
+  state = {};
+
+  handleClick = async () => {
+    const user = await resolveToken(this.props.token);
+    if (user.hasOwnProperty("user")) {
+      this.setState(user);
+    }
+  };
+
+  render() {
+    return (
+      <View>
+        <Title>Connection testing field</Title>
+        <Button onClick={this.handleClick} alternative={true}>
+          Click to ping
+        </Button>
+        {!!this.state.user && <Text>{this.state.user}</Text>}
+        {!!this.state.time && <Text>{this.state.time}</Text>}
+      </View>
+    );
+  }
+}
