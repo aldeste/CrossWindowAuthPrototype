50a06408dd385e372cefe048e58021376525ff21
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-04 15:26:28 +0200

    Made nodeDefenitions testable By extracting nodeDefenitions arguments into functions, each function is easier to test and we can be sure to return the expected result

--- src/schema/relayNode.js
+++ src/schema/relayNode.js
@@ -1,22 +1,24 @@
+// @flow
 import { fromGlobalId, nodeDefinitions } from "graphql-relay";
 
 import personType from "./types/personType";
 import { getObjectFromTypeAndId } from "./apiHelper";
 
-const { nodeInterface, nodeField } = nodeDefinitions(
-  (globalId, ctx) => {
-    const { type, id } = fromGlobalId(globalId);
-    if (type === "Person") {
-      return getObjectFromTypeAndId(personType, id, ctx);
-    }
-    return null;
-  },
-  obj => {
-    if (obj.GraphQLType === "personType") {
-      return personType;
-    }
-    return null;
+export function idFetcher(globalId: string, context?: Object): Object | null {
+  const { type, id } = fromGlobalId(globalId);
+  if (type === "Person") {
+    return getObjectFromTypeAndId(personType, id, context);
   }
-);
+  return null;
+}
+
+export function typeResolver(object: { GraphQLType?: Object }): Object | null {
+  if (object.GraphQLType === personType.name) {
+    return personType;
+  }
+  return null;
+}
+
+const { nodeInterface, nodeField } = nodeDefinitions(idFetcher, typeResolver);
 
 export { nodeInterface, nodeField };
