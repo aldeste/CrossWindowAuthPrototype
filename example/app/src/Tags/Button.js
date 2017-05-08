acbf2b2672b0a5d9aa8d972527b5c51259dade6e
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-08 15:32:23 +0200

    Add outline style

--- example/app/src/Tags/Button.js
+++ example/app/src/Tags/Button.js
@@ -1,24 +1,27 @@
 // @flow
 import styled from "styled-components";
 
+type Props = { outlined: boolean, alternative: boolean };
+
 const Button = styled.button`
   transition: background 100ms linear;
-  color: white;
-  border: 0;
+  color: ${({ outlined }: Props) => (outlined ? "inherit" : "white")};
+  border: ${({ alternative, outlined }: Props) => (outlined ? `3px solid ${!alternative ? "hsl(161,97%,38%)" : "hsl(289,65%,52%)"}` : 0)};
   font-family: inherit;
   font-weight: inherit;
   font-size: inherit;
   line-height: inherit;
-  padding: 0.5rem 1rem;
-  background-color: transparent;
-  background: ${({ alternative }: { alternative: boolean }) => (!alternative ? "hsl(161,97%,38%)" : "hsl(289,65%,52%)")};
+  padding: ${({ outlined }: Props) => (outlined ? "calc(0.5rem - 3px) calc(1rem - 3px)" : "0.5rem 1rem")};
+  background-color: ${({ alternative, outlined }: Props) => (outlined ? "transparent" : !alternative ? "hsl(161,97%,38%)" : "hsl(289,65%,52%)")};
 
   &:hover {
-    background: ${({ alternative }: { alternative: boolean }) => (!alternative ? `hsl(161,97%,${38 - 5}%)` : `hsl(289,65%,${52 - 5}%)`)};
+    background-color: ${({ alternative, outlined }: Props) => (outlined ? "transparent" : !alternative ? `hsl(161,97%,${38 - 5}%)` : `hsl(289,65%,${52 - 5}%)`)};
+    border: ${({ alternative, outlined }: Props) => (outlined ? `3px solid ${!alternative ? `hsl(161,97%,${38 - 5}%)` : `hsl(289,65%,${52 - 5}%)`}` : 0)};
   }
 
   &:disabled {
-    background: ${({ alternative }: { alternative: boolean }) => (!alternative ? `hsl(161,${97 - 40}%,${38 - 5}%)` : `hsl(289,${65 - 40}%,${52 - 5}%)`)};
+    background-color: ${({ alternative, outlined }: Props) => (outlined ? "transparent" : !alternative ? `hsl(161,${97 - 40}%,${38 - 5}%)` : `hsl(289,${65 - 40}%,${52 - 5}%)`)};
+    border: ${({ alternative, outlined }: Props) => (outlined ? `3px solid ${!alternative ? `hsl(161,${97 - 40}%,${38 - 5}%)` : `hsl(289,${65 - 40}%,${52 - 5}%)`}` : 0)};
   }
 
   &:not(:disabled) {
