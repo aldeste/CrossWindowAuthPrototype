e795a40d4dd43fb0cfcf91ee627877c22f26707a
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-08 17:17:37 +0200

    Add message handler tests

--- example/app/src/App.test.js
+++ example/app/src/App.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import App from "./App";
 import renderer from "react-test-renderer";
 
+const addEventListener = jest.fn();
+
 jest
   .mock("./Document/Title", () => ({ children }) => <title>{children}</title>)
   .mock("./Tags/Button", () => ({ className, children, ...props }) => (
@@ -26,14 +28,73 @@ jest
     <div {...props}>{children}</div>
   ));
 
-it("renders without crashing", () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe("Application start file", () => {
+  global.window = {
+    addEventListener: (type, callback, options) =>
+      addEventListener(type, callback, options)
+  };
 
-it("Updates satate to reflect login", () => {
-  const component = renderer.create(<App />);
-  component.getInstance().signIn("StarWars")({ name: "Yoda" });
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  global.console.log = jest.fn();
+
+  it("renders without crashing", () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Renders the welcome screen on login", () => {
+    const component = renderer.create(<App />);
+    component.getInstance().handleLogin("StarWars")({ name: "Yoda" });
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Removes the welcome screen on logout", () => {
+    const component = renderer.create(<App />);
+    component.getInstance().handleLogin("StarWars")({ name: "Yoda" });
+    component.getInstance().handleLogOut("StarWars")();
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Adds an eventlistener for messages on mount", () => {
+    const component = renderer.create(<App />);
+    expect(addEventListener).toHaveBeenCalledWith(
+      "message",
+      component.getInstance().receiveMessage,
+      false
+    );
+  });
+
+  it("Processes messages with correct origin and type", () => {
+    const component = renderer.create(<App />);
+    const message = component
+      .getInstance()
+      .receiveMessage({
+        origin: "http://localhost:4000",
+        data: { type: "AuthVerificationConnection", data: true }
+      });
+    expect(message).toBeTruthy();
+  });
+
+  it("Doesn't process on wrong origin", () => {
+    const component = renderer.create(<App />);
+    const message = component
+      .getInstance()
+      .receiveMessage({
+        origin: "http://localhost:4",
+        data: { type: "AuthVerificationConnection", data: true }
+      });
+    expect(message).toBeFalsy();
+  });
+
+  it("Doesn't process on wrong origin", () => {
+    const component = renderer.create(<App />);
+    const message = component
+      .getInstance()
+      .receiveMessage({
+        origin: "http://localhost:4",
+        data: { type: "NotCorrect", data: true }
+      });
+    expect(message).toBeFalsy();
+  });
 });
