e0776bc543bef73a80fea96c4f0a35e0283cce2d
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-08 15:31:59 +0200

    Add immutable, add signout

--- example/app/src/App.js
+++ example/app/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import Wrapper from "./Tags/Wrapper";
 import DocumentTitle from "./Document/Title";
 import Login from "./Login/Login";
+import Welcome from "./Welcome/Welcome";
+import { Map, type Map as ImmutableMap } from "immutable";
 import Ping from "./PingTest/Ping";
 
 // type MessageEventWithOptions = {
@@ -10,7 +12,14 @@ import Ping from "./PingTest/Ping";
 //   data: { type: string, data: string }
 // };
 
-class App extends React.Component<*, *, *> {
+type State = {
+  signedIn: ImmutableMap<string, ImmutableMap<string, string>>
+};
+
+class App extends React.Component<*, State, *> {
+  state = {
+    signedIn: Map()
+  };
   // receiveMessage = (event: MessageEventWithOptions) => {
   //   if (event.origin !== "http://localhost:4000") return;
   //   if (event.data.type === "AuthVerificationConnection") {
@@ -22,24 +31,45 @@ class App extends React.Component<*, *, *> {
   //   window && window.addEventListener("message", this.receiveMessage, false);
   // }
 
-  signIn = (formName: string) => (props: Object) => {
-    this.setState(() => ({ [formName]: { props } }));
+  signIn = (formName: string): Function => (props: Object): void => {
+    this.setState(() => ({
+      signedIn: this.state.signedIn.set(formName, Map(props))
+    }));
+  };
+
+  handleLogOut = (logoutName: string): Function => (
+    event: SyntheticEvent
+  ): void => {
+    this.setState(() => ({
+      signedIn: this.state.signedIn.remove(logoutName)
+    }));
   };
 
   render() {
+    const UserBases: Array<string> = ["StarWars", "StarTrek"];
+    const { signedIn } = this.state;
+
     return (
       <Wrapper>
         <DocumentTitle>This is a demo page</DocumentTitle>
-        {this.state && <p>{JSON.stringify(this.state, null, 2)}</p>}
-        <Ping token="Hello" />
-        <Login
-          title="Star Wars login"
-          onLoginSubmit={this.signIn("StarWars")}
-        />
-        <Login
-          title="Breaking Bad login"
-          onLoginSubmit={this.signIn("BreakingBad")}
-        />
+        {UserBases.some(userBase => signedIn.has(userBase)) &&
+          <p>{JSON.stringify(signedIn, null, 2)}</p>}
+        <Ping token="4" />
+        {UserBases.map(
+          part =>
+            signedIn.has(part)
+              ? <Welcome
+                  key={part}
+                  title={`${part.replace(/([a-z])([A-Z])/g, "$1 $2")} user logged in!`}
+                  username={signedIn.getIn([part, "name"])}
+                  handleLogOut={this.handleLogOut(part)}
+                />
+              : <Login
+                  key={part}
+                  title={`${part.replace(/([a-z])([A-Z])/g, "$1 $2")} login`}
+                  onLoginSubmit={this.signIn(part)}
+                />
+        )}
       </Wrapper>
     );
   }
