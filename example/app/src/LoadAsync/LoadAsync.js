443be8acfdd12a171ff4f7c7cb0440c11ee222a2
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-12 18:08:47 +0200

    Async loading to own folder

---
+++ example/app/src/LoadAsync/LoadAsync.js
@@ -0,0 +1,25 @@
+// @flow
+import Loadable from "react-loadable";
+import React from "react";
+
+type Props = {
+  isLoading: boolean,
+  error: boolean,
+  pastDelay: boolean
+};
+
+export default function LoadAsync(opts: Object) {
+  return Loadable({
+    LoadingComponent({ isLoading, error, pastDelay }: Props) {
+      if (isLoading && pastDelay) {
+        return <div>Loading...</div>;
+      }
+      if (error) {
+        return <div>Error! Component failed to load</div>;
+      }
+      return null;
+    },
+    delay: 200,
+    ...opts
+  });
+}
