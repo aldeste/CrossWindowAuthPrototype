07917461599076bf3798c57e5a4f21b4ef4530f5
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-17 14:27:55 +0200

    More consistent test

--- example/app/src/Document/Styles.test.js
+++ example/app/src/Document/Styles.test.js
@@ -1,13 +1,25 @@
-import setGlobalStyles from "./Styles";
+// jest.mock("styled-components", () => ({
+//   injectGlobal: () => jest.fn()
+// }));
+
+const mockFunctionInjectGlobal = jest.fn();
+jest.mock("styled-components", () => {
+  return {
+    injectGlobal: args => mockFunctionInjectGlobal(args)
+  };
+});
 
 describe("Styles", () => {
+  const InjectGlobalStyles = require("./Styles").default;
   it("Matches previous settings", () => {
-    expect(setGlobalStyles.toString()).toMatchSnapshot();
+    // expect(setGlobalStyles.toString()).toMatchSnapshot();
+    InjectGlobalStyles();
+    expect(mockFunctionInjectGlobal.mock.calls[0]).toMatchSnapshot();
   });
   it("Runs without crashing", () => {
-    expect(() => setGlobalStyles()).not.toThrowError();
+    expect(() => InjectGlobalStyles()).not.toThrowError();
   });
   it("Returns void", () => {
-    expect(setGlobalStyles()).toBeUndefined();
+    expect(InjectGlobalStyles()).toBeUndefined();
   });
 });
