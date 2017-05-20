17045e9430f315df9aa0b8c5da0ce99f89e87f2b
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-20 14:19:01 +0200

    Change name of database

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ const DB_HOST = process.env.DB_HOST || "127.0.0.1";
 const DB_PORT = process.env.DB_PORT || "3306";
 const DB_USER = process.env.DB_USER || "root";
 const DB_PASS = process.env.DB_PASS || "password";
-const DB_NAME = process.env.DB_NAME || "development";
+const DB_NAME = process.env.DB_NAME || "authjazz";
 
 module.exports = {
   APP_PORT,
