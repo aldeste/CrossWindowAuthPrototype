82a47f541ce2607c688291b3345816dc2f461647
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-20 17:25:55 +0200

    Add tables to database

--- src/database/models/person.js
+++ src/database/models/person.js
@@ -3,11 +3,17 @@ import { STRING, INTEGER, VIRTUAL } from "sequelize";
 
 const Person = database.define("person", {
   id: { type: INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: STRING, allowNull: false },
-  password: { type: STRING, allowNull: false },
-  token: { type: STRING, allowNull: false },
   birthYear: { type: STRING, allowNull: false },
   eyeColor: { type: STRING, allowNull: false },
+  gender: { type: STRING },
+  hairColor: { type: STRING },
+  height: { type: INTEGER },
+  mass: { type: INTEGER },
+  name: { type: STRING, allowNull: false },
+  skinColor: { type: STRING },
+  // Customs
+  password: { type: STRING, allowNull: false },
+  token: { type: STRING, allowNull: false },
   GraphQLType: {
     type: VIRTUAL,
     get: () => "Person"
