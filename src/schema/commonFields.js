53d7449c1c1220040f5b272cbceaa495b2229764
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-20 09:45:47 +0200

    Remove unneeded resolve

--- src/schema/commonFields.js
+++ src/schema/commonFields.js
@@ -4,15 +4,13 @@ import { GraphQLString, type GraphQLFieldConfig } from "graphql";
 export function createdField(): GraphQLFieldConfig<*, *> {
   return {
     type: GraphQLString,
-    description: "The ISO 8601 date format of the time that this resource was created.",
-    resolve: type => type.createdAt
+    description: "The ISO 8601 date format of the time that this resource was created."
   };
 }
 
 export function editedField(): GraphQLFieldConfig<*, *> {
   return {
     type: GraphQLString,
-    description: "The ISO 8601 date format of the time that this resource was edited.",
-    resolve: type => type.editedAt
+    description: "The ISO 8601 date format of the time that this resource was edited."
   };
 }
