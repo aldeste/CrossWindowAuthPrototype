03d69896adc79e0df33912b00cd5c51189a000c6
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-22 16:52:58 +0200

    Iframe as async loadable

--- example/app/src/Tags/index.js
+++ example/app/src/Tags/index.js
@@ -3,6 +3,7 @@ import LoadAsync from "../LoadAsync/LoadAsync";
 
 const Button = LoadAsync({ loader: () => import("./Button") });
 const Form = LoadAsync({ loader: () => import("./Form") });
+const Iframe = LoadAsync({ loader: () => import("./Iframe") });
 const Label = LoadAsync({ loader: () => import("./Label") });
 const Text = LoadAsync({ loader: () => import("./Text") });
 const TextInput = LoadAsync({ loader: () => import("./TextInput") });
@@ -10,4 +11,4 @@ const Title = LoadAsync({ loader: () => import("./Title") });
 const View = LoadAsync({ loader: () => import("./View") });
 const Wrapper = LoadAsync({ loader: () => import("./Wrapper") });
 
-export { Button, Form, Label, Text, TextInput, Title, View, Wrapper };
+export { Button, Form, Iframe, Label, Text, TextInput, Title, View, Wrapper };
