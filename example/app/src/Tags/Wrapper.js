cce238e45da541cd73ff15107b42e0aa23bf3493
Alexander D'Este <alexanderdeste@gmail.com>
2017-05-22 16:52:39 +0200

    Padding instead of margin to expand background color

--- example/app/src/Tags/Wrapper.js
+++ example/app/src/Tags/Wrapper.js
@@ -3,12 +3,13 @@ import View from "./View";
 
 const Wrapper = styled(View)`
   color: #333;
+  background: #fff;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   font-size: 17px;
   font-weight: 400;
   letter-spacing: -0.021em;
   line-height: 1.52947;
-  margin: 2rem 5rem;
+  padding: 2rem 5rem;
 
   > * + * {
     margin-top: 2rem;
